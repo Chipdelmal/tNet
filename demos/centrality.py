6847f6f4e50e9374e72f442e5c953fe33ddb86ca
chipdelmal <chipdelmal@gmail.com>
2019-10-31 21:06:11 -0700

    Adding "cache" to gitignore

--- demos/centrality.py
+++ demos/centrality.py
@@ -3,7 +3,7 @@ import matplotlib.colors as colors
 import networkx as nx
 import osmnx as ox
 import pandas as pd
-%matplotlib inline
+
 ox.config(log_console=True, use_cache=True)
 ox.__version__
 
@@ -21,4 +21,3 @@ df = df.reindex(G.nodes())
 nc = df['colors'].tolist()
 fig, ax = ox.plot_graph(G, bgcolor='k', node_size=30, node_color=nc, node_edgecolor='none', node_zorder=2,
                         edge_color='#555555', edge_linewidth=1.5, edge_alpha=1)
-                        
